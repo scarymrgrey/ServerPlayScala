e1fc874753a7f01d15a5181034730e596e665f9b
Scary <imbamrgrey@gmail.com>
2018-06-11 00:53:44 +0300

    game creation case

--- app/controllers/BaseController.scala
+++ app/controllers/BaseController.scala
@@ -8,43 +8,39 @@ import Operations.Entity.{Session, User}
 import Operations.Query.{GetEntityById, GetSessionByUUIDQuery}
 import net.liftweb.json.DefaultFormats
 import net.liftweb.json.Serialization.write
-import play.api.mvc.Results.Forbidden
-import play.api.mvc._
+import play.api.libs.json.JsValue
+import play.api.mvc.{Action, _}
 
-case class AuthRequest(user: User, request: Request[AnyContent])
+case class AuthRequest(user: User, request: Request[JsValue])
   extends WrappedRequest(request)
 
-trait AuthController {
-  def Authenticated(action: AuthRequest => Result): Action[AnyContent] = {
-    Action { request =>
-      val forbid = Forbidden("You're not allowed to access this resource.")
-      request.headers.get("session") match {
-        case Some(session) => Dispatcher.Query(GetSessionByUUIDQuery(session.toString)) match {
-          case Some(s) => {
-            val tenSecBeforeNow = Calendar.getInstance()
-            tenSecBeforeNow.add(Calendar.SECOND, -200)
-            if (s.CDate.before(tenSecBeforeNow.getTime)) {
-              Dispatcher.Push(DeleteEntityCommand[Session](s._id))
-              forbid
-            } else {
-              Dispatcher.Push(ExtendSessionCommand(s._id))
-              Dispatcher.Query(GetEntityById[User](s.UserId))
-                .map(user => action(AuthRequest(user, request))).get
-            }
-          }
-          case None => forbid
-        }
-        case None => forbid
-      }
-    }
-  }
-}
-
-abstract class BaseController(cc: ControllerComponents) extends AbstractController(cc) with AuthController {
+abstract class BaseController(cc: ControllerComponents) extends AbstractController(cc)  {
   implicit val formats = DefaultFormats
 
   def HttpOk[T](res: T) = {
     Ok(write(res))
+  }
+
+  def Authenticated(action: AuthRequest => Result): Action[JsValue] = Action(parse.json) {implicit request =>
+    val forbid = Forbidden("You're not allowed to access this resource.")
+    request.headers.get("session") match {
+      case Some(session) => Dispatcher.Query(GetSessionByUUIDQuery(session.toString)) match {
+        case Some(s) => {
+          val tenSecBeforeNow = Calendar.getInstance()
+          tenSecBeforeNow.add(Calendar.SECOND, -200)
+          if (s.CDate.before(tenSecBeforeNow.getTime)) {
+            Dispatcher.Push(DeleteEntityCommand[Session](s._id))
+            forbid
+          } else {
+            Dispatcher.Push(ExtendSessionCommand(s._id))
+            Dispatcher.Query(GetEntityById[User](s.UserId))
+              .map(user => action(AuthRequest(user, request))).get
+          }
+        }
+        case None => forbid
+      }
+      case None => forbid
+    }
   }
 
 }
