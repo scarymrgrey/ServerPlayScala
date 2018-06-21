449fa0e0798c721b6391a19099ca0185c1b272c1
kirill.polunin <scarymrgrey>
2018-06-21 18:58:40 +0300

    fix

--- app/Operations/Query/GetUserByNameQuery.scala
+++ app/Operations/Query/GetUserByNameQuery.scala
@@ -3,7 +3,7 @@ package Operations.Query
 import java.util.Calendar
 
 import CQRS.Base.QueryBase
-import Operations.Entity.{Session, User}
+import Operations.Entity.{Game, Session, User}
 import com.mongodb.casbah.commons.MongoDBObject
 
 case class Response(username: String, online: Boolean, wins: Int, losses: Int)
@@ -16,6 +16,7 @@ final case class GetUserByNameQuery(login: String) extends QueryBase[Option[Resp
         tenSecBeforeNow.add(Calendar.SECOND, -10)
         val session = Repository.GetSome[Session](MongoDBObject("UserId" -> usr._id)) headOption
         val active  = for (x <- session) yield x.CDate.before(tenSecBeforeNow.getTime)
+       // val gamesWin = Repository.GetSome[Game](MongoDBObject("won" -> login,"finished"-> true)).count
         Option(Response(usr.login, active.getOrElse(false), 0, 0))
 
       case None => None
--- app/Operations/Query/GetUsersByFilterQuery.scala
+++ app/Operations/Query/GetUsersByFilterQuery.scala
@@ -1,14 +1,14 @@
 package Operations.Query
 
-import CQRS.Base.QueryBase
+import CQRS.Base.{Dispatcher, QueryBase}
 import Operations.Entity.User
 
 
-final case class GetUsersByFilterQuery(limit: Int,offset : Int) extends QueryBase[Seq[User]] {
+final case class GetUsersByFilterQuery(limit: Int, offset: Int) extends QueryBase[Seq[User]] {
   override def ExecuteResult(): Seq[User] = {
-    require(limit>0)
-    require(offset>=0)
-    val users = Repository.GetAll[User]().slice(offset, offset + limit)
+    require(limit > 0)
+    require(offset >= 0)
+    val users = Repository.GetAll[User]().map(r => Dispatcher.Query(GetUserByNameQuery(r.login))).slice(offset, offset + limit)
     users
   }
 }
