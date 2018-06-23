f3c0761bea95008f0483a4f77c2f978fc03de6d2
Scary <imbamrgrey@gmail.com>
2018-06-23 19:03:33 +0300

    build fix

--- app/Operations/Query/GetUserByNameQuery.scala
+++ app/Operations/Query/GetUserByNameQuery.scala
@@ -6,10 +6,10 @@ import CQRS.Base.QueryBase
 import Operations.Entity.{Game, Session, User}
 import com.mongodb.casbah.commons.MongoDBObject
 
-case class Response(username: String, online: Boolean, wins: Int, losses: Int)
+case class UserResponse(username: String, online: Boolean, wins: Int, losses: Int)
 
-final case class GetUserByNameQuery(login: String) extends QueryBase[Option[Response]] {
-  override def ExecuteResult(): Option[Response] = {
+final case class GetUserByNameQuery(login: String) extends QueryBase[Option[UserResponse]] {
+  override def ExecuteResult(): Option[UserResponse] = {
     Repository.GetSome[User](MongoDBObject("login" -> login)).headOption match {
       case Some(usr) =>
         val tenSecBeforeNow = Calendar.getInstance()
@@ -17,7 +17,7 @@ final case class GetUserByNameQuery(login: String) extends QueryBase[Option[Resp
         val session = Repository.GetSome[Session](MongoDBObject("UserId" -> usr._id)) headOption
         val active  = for (x <- session) yield x.CDate.before(tenSecBeforeNow.getTime)
        // val gamesWin = Repository.GetSome[Game](MongoDBObject("won" -> login,"finished"-> true)).count
-        Option(Response(usr.login, active.getOrElse(false), 0, 0))
+        Option(UserResponse(usr.login, active.getOrElse(false), 0, 0))
 
       case None => None
     }
--- app/Operations/Query/GetUsersByFilterQuery.scala
+++ app/Operations/Query/GetUsersByFilterQuery.scala
@@ -3,12 +3,12 @@ package Operations.Query
 import CQRS.Base.{Dispatcher, QueryBase}
 import Operations.Entity.User
 
-
-final case class GetUsersByFilterQuery(limit: Int, offset: Int) extends QueryBase[Seq[User]] {
-  override def ExecuteResult(): Seq[User] = {
+final case class GetUsersByFilterQuery(limit: Int, offset: Int) extends QueryBase[Seq[UserResponse]] {
+  override def ExecuteResult(): Seq[UserResponse] = {
     require(limit > 0)
     require(offset >= 0)
-    val users = Repository.GetAll[User]().map(r => Dispatcher.Query(GetUserByNameQuery(r.login))).slice(offset, offset + limit)
-    users
+    Repository.GetAll[User]()
+      .map(r => Dispatcher.Query(GetUserByNameQuery(r.login)).get)
+      .slice(offset, offset + limit)
   }
 }
